ade0d10e977ac36497bbdc6704391983528b840f
Hans Choi <personabe1984@gmail.com>
2016-10-06 11:24:41 -0400

    adding all the stuff I did so far

---
+++ timestrings.py
@@ -0,0 +1,10 @@
+"""Create a function named to_string that takes a datetime and gives
+        back a string in the format "24 September 2012".
+"""
+
+# Examples
+# to_string(datetime_object) => "24 September 2012"
+# from_string("09/24/12 18:30", "%m/%d/%y %H:%M") => datetime
+import datetime
+
+
